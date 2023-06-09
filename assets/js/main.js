987943f3b35f7194b1b1cadbbe11fd3978e41c05
Carlos5998 <carlosrod5998@gmail.com>
2023-06-09 14:37:46 -0500

    FINISH

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,3 +147,22 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/ 
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset; true // Animations repeat
+})
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.category__card, .items__card`, {interval: 100})
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, {origin: 'right'})
+
+
